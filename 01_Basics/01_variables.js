1e08ec1ec972811f50227f17414320a1df6888be
Abhay Gautam <68742118+Eshuv@users.noreply.github.com>
2024-07-30 02:27:46 +0000

    DataTypes and Variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 1575
+let accountEmail = "gautameshuv@gmail.com"
+var accountPassword = "abhay"
+accountCity = "Ghaziabad"
+let demoVariable;  // Gives a undefined value to the variable.
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope.
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, demoVariable])
